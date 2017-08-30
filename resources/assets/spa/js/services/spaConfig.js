58db40d85b5f4373024c9abe1e5a0779952a9f78
Luiz <argentinaluiz@gmail.com>
2017-08-29 22:01:47 -0300

    Versão original dos assets

--- resources/assets/spa/js/services/spaConfig.js
+++ resources/assets/spa/js/services/spaConfig.js
@@ -1,8 +1,8 @@
 const location = window.location;
 
 export default {
-    //HOST: `${location.protocol}//${location.hostname}:${location.port}`,
-    HOST: 'http://br326.teste.website/~devol190',
+    HOST: `${location.protocol}//${location.hostname}:${location.port}`,
+    //HOST: 'http://br326.teste.website/~devol190',
     get API_URL(){
         return `${this.HOST}/api`;
     },
